75215a8c72be9371dfefa09e4105cd359d452d8c
Akhil Jain <akhil.jain93@gmail.com>
2016-06-03 19:21:52 +0530

    show 3 views on master

--- entity_score/gen_master.py
+++ entity_score/gen_master.py
@@ -8,13 +8,13 @@ def getAllEntitiesAndUrls(cascade, filename):
 
 foreign_lang_cascades = [3, 8, 15, 22, 33, 42]
 
-print '<!DOCTYPE html>\n<html>\n  <head>\n    <meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\"/>\n  </head>\n  <body>\n    <h1>Twitter Cascade Summarisation</h1>\n    <table border=\"1\" style=\"width:100%\">\n      <tr>\n        <th>Cascade</th>\n        <th>Major entities</th>\n      </tr>'
+print '<!DOCTYPE html>\n<html>\n  <head>\n    <meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\"/>\n  </head>\n  <body>\n    <h1>Twitter Cascade Summarisation</h1>\n    <table border=\"1\" style=\"width:100%\">\n      <tr>\n        <th>Cascade</th>\n        <th>Cascade views</th>\n        <th>Major entities</th>\n      </tr>'
 for i in range(0, 51):
     if i not in foreign_lang_cascades:
         f = open('reps/rep_' + str(i) + '.csv', 'r').read().splitlines()
-        print '      <tr>\n        <td><a href=index.html?cascade=' + str(i) + '>Cascade ' + str(i) + '</a></td>\n        <td>' + getAllEntitiesAndUrls(i, f[1:]) + '</td>\n      </tr>'
+        print '      <tr>\n        <td>' + str(i) + '</td>\n        <td><a href=index.html?cascade=' + str(i) + '&view=1>View 1</a> <a href=index.html?cascade=' + str(i) + '&view=2>View 2</a> <a href=index.html?cascade=' + str(i) + '&view=3>View 3</a></td>\n        <td>' + getAllEntitiesAndUrls(i, f[1:]) + '</td>\n      </tr>'
     else:
-        print '      <tr>\n        <td><a href=index.html?cascade=' + str(i) + '>Cascade ' + str(i) + '</a></td>\n        <td>Foreign Language Cascade</td>\n      </tr>'
+        print '      <tr>\n        <td>' + str(i) + '</td>\n        <td><a href=index.html?cascade=' + str(i) + '&view=1>View 1</a> <a href=index.html?cascade=' + str(i) + '&view=2>View 2</a> <a href=index.html?cascade=' + str(i) + '&view=3>View 3</a></td>\n        <td>Foreign Language Cascade</td>\n      </tr>'
 
 
 print '    </table>\n  </body>\n</html>'
