1be293eb69e1dc90b9f62849522ade2b087dc8af
Akhil Jain <akhil.jain93@gmail.com>
2016-06-08 02:14:34 +0530

    connect to mysql database

--- evaluation/evaluation.php
+++ evaluation/evaluation.php
@@ -9,7 +9,50 @@
     </div>
     <?php
     if (isset($_POST['submit'])) {
-    // TODO: put all the data into the database
+        $servername = "localhost";
+        $user = "root";
+        $password = "root";
+        $database = "evaluation_data";
+        $conn = mysql_connect($servername, $user, $password);
+
+        if (!$conn) {
+            die("Could not connect: ".mysql_error());
+        }
+
+        if (! get_magic_quotes_gpc()) {
+            $name = addslashes($_POST['name']);
+            $email = addslashes($_POST['email']);
+            $summary = addslashes($_POST['summary']);
+            $user_interface = addslashes($_POST['ui']);
+            $presentation = addslashes($_POST['presentation']);
+        } else {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $summary = $_POST['summary'];
+            $user_interface = $_POST['ui'];
+            $presentation = $_POST['presentation'];
+        }
+
+        $cascade = intval($_GET['cascade']);
+
+        $values = "$cascade, '$name', '$email', '$presentation', '$summary', '$user_interface'";
+        $columns = "cascade_no, name, email, presentation, summary, user_interface";
+
+        for ($x = 1; $x <= 10; $x++) {
+            $tweets = intval($_POST['tweet_'.$x]);
+            $entities = intval($_POST['entity_'.$x]);
+            $columns = $columns.", entity_".$x.", entity_tweet_".$x;
+            $values = $values.", $entities, $tweets";
+        }
+
+        mysql_select_db($database) or die("Unable to select database");
+        $insert_query = "insert into results($columns) values ($values)";
+        $retval = mysql_query($insert_query, $conn);
+        if (!$retval) {
+            die('Could not enter data: '.mysql_error());
+        }
+
+        mysql_close($conn);
     ?>
     <h3>Thank you for the evaluation. We will be in touch.</h3>
     <?php
@@ -21,18 +64,8 @@
             <input type="text" name="name" class="form-control" required>
         </div>
         <div class="form-group">
-            <label for="cascade">Cascade number</label>
-            <select name="cascade" class="form-control">
-            <?php
-                for ($x = 0; $x <= 50; $x++) {
-                    echo '<option value="'.$x.'"';
-                    if($_GET['cascade'] == $x) {
-                        echo ' selected="selected"';
-                    }
-                    echo '>'.$x.'</option>';
-                }
-            ?>
-             </select>
+            <label for="email">Email</label>
+            <input type="text" name="email" class="form-control" required>
         </div>
         <fieldset>
             <legend>Keywords - Rate the importance</legend>
@@ -44,8 +77,8 @@
                     ?>
                         <div class="form-group">
                     <?php
-                        echo '<label for="entity_'.($row - 1).'">'.explode(".", $data[1])[1].'</label>';
-                        echo '<select name="entity_'.($row - 1).'" class="form-control">';
+                        echo '<label for="entity_'.$row.'">'.explode(".", $data[1])[1].'</label>';
+                        echo '<select name="entity_'.$row.'" class="form-control">';
                         for ($x = 5; $x > 0; $x--) {
                             echo '<option value="'.$x.'">'.$x.'</option>';
                         }
@@ -69,12 +102,12 @@
                     ?>
                         <div class="form-group">
                     <?php
-                        echo '<label for="tweet_'.($row - 1).'">'.explode(".", $data[1])[1].' - "';
+                        echo '<label for="tweet_'.$row.'">'.explode(".", $data[1])[1].' - "';
                         for ($x = 3; $x < count($data); $x++) {
                             echo $data[$x];
                         }
                         echo '"</label>';
-                        echo '<select name="tweet_'.($row - 1).'" class="form-control">';
+                        echo '<select name="tweet_'.$row.'" class="form-control">';
                         echo '<option value="correct">Correct</option>';
                         echo '<option value="incorrect">Incorrect</option>';
                     ?>
